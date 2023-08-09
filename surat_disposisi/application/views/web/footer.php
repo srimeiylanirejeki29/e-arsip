9480388e097c10f5ad51d8db8a46ba1a1e2f36de
srimeiylanirejeki29 <93021732+srimeiylanirejeki29@users.noreply.github.com>
2023-08-09 17:45:21 +0700

    E-arsip
    sistem informasi e-arsip PT Bonet Utama

---
+++ surat_disposisi/application/views/web/footer.php
@@ -0,0 +1,16 @@
+<!-- Footer -->
+<div class="footer text-muted text-center">
+	<a href="" class="text-danger"><b>SIS Administrasi</b></a> &copy; 2023
+</div>
+<!-- /footer -->
+</div>
+<!-- /content area -->
+</div>
+<!-- /main content -->
+</div>
+<!-- /page content -->
+</div>
+<!-- /page container -->
+</body>
+
+</html>
